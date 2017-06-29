f1817d6b023dbb56e4e7b7e2a1cedd40dcb4afae
David Mallard <correspondence@davidmallard.id.au>
2017-06-29 16:54:26 +1000

    Add a deploy task to publish the website to the gh-pages branch

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var deploy = require('gulp-gh-pages');
 
 gulp.task('build', function() {
 	return gulp.src('src/**/*')
@@ -12,4 +13,13 @@ gulp.task('browser-sync', function() {
 			baseDir: 'dist'
 		}
 	});
+});
+
+/**
+ * Push build to gh-pages:
+ * https://medium.com/superhighfives/deploying-to-github-pages-with-gulp-c06efc527de8
+ */
+gulp.task('deploy', function () {
+	return gulp.src("dist/**/*")
+	.pipe(deploy())
 });
