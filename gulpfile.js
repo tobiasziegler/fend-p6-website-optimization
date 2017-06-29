03aaefffee3f58a5f534751c8e235269206380cd
David Mallard <correspondence@davidmallard.id.au>
2017-06-29 16:26:44 +1000

    Add BrowserSync as a built-in test server

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 
 gulp.task('build', function() {
 	return gulp.src('src/**/*')
 	.pipe(gulp.dest('dist'));
+});
+
+gulp.task('browser-sync', function() {
+	browserSync.init({
+		server: {
+			baseDir: 'dist'
+		}
+	});
 });
